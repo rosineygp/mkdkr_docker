eb8a80e9db1f93e66be461b34410b0ccbdd10568
Rosiney Gomes Pereira <rosiney.gp@gmail.com>
2020-02-01 23:40:02 -0300

    fix: command name

--- main.mk
+++ main.mk
@@ -3,5 +3,5 @@ TAG=latest
 define docker_build =
 	$(.)
 	... privileged docker:19
-	.. docker built -t $(REGISTRY)/$(PROJECT)/$(REPOS):$(TAG) .
+	.. docker build -t $(REGISTRY)/$(PROJECT)/$(REPOS):$(TAG) .
 endef
